01effcc3b444f6c5f3f9a9c63d44aa147ce4829d
nickderaj <nickderaj@gmail.com>
2023-04-30 12:32:10 +0800

    fix: prevent numbers over 9 and below 0

--- src/components/Board/BoardInput.tsx
+++ src/components/Board/BoardInput.tsx
@@ -5,6 +5,13 @@ import Board from './Board';
 const BoardInput: React.FC = () => {
   const [board, setBoard] = useState(EMPTY_BOARD);
   const onCellChange = (row: number, col: number, value: number) => {
+    if (value === undefined || value === null || Number.isNaN(value)) {
+      value = 0;
+    } else if (value < 0) {
+      value = 0;
+    } else if (value > 9) {
+      return;
+    }
     const newBoard = board.map((rowArr, rowIndex) => {
       if (rowIndex === row) {
         return rowArr.map((cellVal, colIndex) => {
